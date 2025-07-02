2913e03a37223ebac11f5e35c91582e799e2f8f5
enamulhimel <mhasan173013@bscse.uiu.ac.bd>
2025-07-02 15:06:56 +0600

    task context added

--- src/context/TaskContext.tsx
+++ src/context/TaskContext.tsx
@@ -1,34 +1,34 @@
-// 'use client';
+'use client';
 
-// import { createContext, useState, ReactNode } from 'react';
-// import { Task, TaskContextType } from '@/lib/types';
+import { createContext, useState, ReactNode } from 'react';
+import { Task, TaskContextType } from '@/lib/types';
 
-// export const TaskContext = createContext<TaskContextType | undefined>(undefined);
+export const TaskContext = createContext<TaskContextType | undefined>(undefined);
 
-// export const TaskProvider = ({ children }: { children: ReactNode }) => {
-//   const [tasks, setTasks] = useState<Task[]>([]);
+export const TaskProvider = ({ children }: { children: ReactNode }) => {
+  const [tasks, setTasks] = useState<Task[]>([]);
 
-//   const addTask = (task: Omit<Task, 'id'>) => {
-//     setTasks([...tasks, { ...task, id: Date.now().toString() }]);
-//   };
+  const addTask = (task: Omit<Task, 'id'>) => {
+    setTasks([...tasks, { ...task, id: Date.now().toString() }]);
+  };
 
-//   const editTask = (id: string, updatedTask: Partial<Task>) => {
-//     setTasks(tasks.map(task => task.id === id ? { ...task, ...updatedTask } : task));
-//   };
+  const editTask = (id: string, updatedTask: Partial<Task>) => {
+    setTasks(tasks.map(task => task.id === id ? { ...task, ...updatedTask } : task));
+  };
 
-//   const deleteTask = (id: string) => {
-//     setTasks(tasks.filter(task => task.id !== id));
-//   };
+  const deleteTask = (id: string) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
 
-//   const addSubtasks = (id: string, subtasks: string[]) => {
-//     setTasks(tasks.map(task => 
-//       task.id === id ? { ...task, subtasks } : task
-//     ));
-//   };
+  const addSubtasks = (id: string, subtasks: string[]) => {
+    setTasks(tasks.map(task => 
+      task.id === id ? { ...task, subtasks } : task
+    ));
+  };
 
-//   return (
-//     <TaskContext.Provider value={{ tasks, addTask, editTask, deleteTask, addSubtasks }}>
-//       {children}
-//     </TaskContext.Provider>
-//   );
-// };
+  return (
+    <TaskContext.Provider value={{ tasks, addTask, editTask, deleteTask, addSubtasks }}>
+      {children}
+    </TaskContext.Provider>
+  );
+};
