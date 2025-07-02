2131b56dca0ed0ae4726ca5aab435c64ce00d9f6
enamulhimel <mhasan173013@bscse.uiu.ac.bd>
2025-07-02 15:06:03 +0600

    task types added

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,16 +1,16 @@
-// export interface Task {
-//   id: string;
-//   title: string;
-//   description: string;
-//   status: 'pending' | 'completed';
-//   dueDate: string;
-//   subtasks?: string[];
-// }
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+  status: 'pending' | 'completed';
+  dueDate: string;
+  subtasks?: string[];
+}
 
-// export interface TaskContextType {
-//   tasks: Task[];
-//   addTask: (task: Omit<Task, 'id'>) => void;
-//   editTask: (id: string, updatedTask: Partial<Task>) => void;
-//   deleteTask: (id: string) => void;
-//   addSubtasks: (id: string, subtasks: string[]) => void;
-// }
+export interface TaskContextType {
+  tasks: Task[];
+  addTask: (task: Omit<Task, 'id'>) => void;
+  editTask: (id: string, updatedTask: Partial<Task>) => void;
+  deleteTask: (id: string) => void;
+  addSubtasks: (id: string, subtasks: string[]) => void;
+}
